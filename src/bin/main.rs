a1a1986e2694eb434fb3c92e5675f5fd0dcde6ce
Romain Delhommais <romain@delhommais.com>
2026-07-29 00:09:45 +0200

    feat(circuit): add circuit 3

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,9 +5,12 @@ use esp_backtrace as _;
 use esp_hal::analog::adc::{Adc, AdcConfig, Attenuation};
 use esp_hal::clock::CpuClock;
 use esp_hal::gpio::{Input, InputConfig, Level, Output, OutputConfig};
+use esp_hal::ledc::channel::{self, ChannelIFace};
+use esp_hal::ledc::timer::{self, TimerIFace};
+use esp_hal::ledc::{LSGlobalClkSource, Ledc, LowSpeed};
 use esp_hal::main;
 use esp_hal::peripherals::Peripherals;
-use esp_hal::time::{Duration, Instant};
+use esp_hal::time::{Duration, Instant, Rate};
 use log::info;
 
 esp_bootloader_esp_idf::esp_app_desc!();
@@ -20,7 +23,7 @@ fn main() -> ! {
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
-    circuit_2_potentiometer(peripherals);
+    circuit_3_pwm(peripherals);
 }
 
 fn circuit_1_button_led(peripherals: Peripherals) -> ! {
@@ -59,5 +62,39 @@ fn circuit_2_potentiometer(peripherals: Peripherals) -> ! {
 
         let delay_start = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(1000) {}
+    }
+}
+
+fn circuit_3_pwm(peripherals: Peripherals) -> ! {
+    let led_pin = peripherals.GPIO4;
+
+    let mut ledc = Ledc::new(peripherals.LEDC);
+    ledc.set_global_slow_clock(LSGlobalClkSource::APBClk);
+
+    let mut timer0 = ledc.timer::<LowSpeed>(timer::Number::Timer0);
+    timer0
+        .configure(timer::config::Config {
+            duty: timer::config::Duty::Duty8Bit,
+            clock_source: timer::LSClockSource::APBClk,
+            frequency: Rate::from_khz(5),
+        })
+        .unwrap();
+
+    let mut channel0 = ledc.channel(channel::Number::Channel0, led_pin);
+    channel0
+        .configure(channel::config::Config {
+            timer: &timer0,
+            duty_pct: 0,
+            drive_mode: esp_hal::gpio::DriveMode::PushPull,
+        })
+        .unwrap();
+
+    loop {
+        info!("starting fade up");
+        channel0.start_duty_fade(0, 100, 1000).unwrap();
+        while channel0.is_duty_fade_running() {}
+        info!("starting fade down");
+        channel0.start_duty_fade(100, 0, 1000).unwrap();
+        while channel0.is_duty_fade_running() {}
     }
 }
